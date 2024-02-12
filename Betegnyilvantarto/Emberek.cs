9915ddb49ad458eaec84c8ddbb115cc14bf32de0
Balu <Balu@DESKTOP-3S5SC9P>
2024-02-12 16:21:59 +0100

    Első változat.

---
+++ Betegnyilvantarto/Emberek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betegnyilvantarto
+{
+    internal class Emberek
+    {
+        public string Nev { get; set; }
+        public DateTime SzulDat { get; set; }
+        public Int32 Eletkor { get; set; }
+        public string Neme { get; set; }
+        public Int32 TestMag { get; set; }
+        public string Suly { get; set; }
+        public string UjPanasz { get; set; }
+    }
+}
--- Betegnyilvantarto/PeopleEditorWindow.xaml.cs
+++ Betegnyilvantarto/PeopleEditorWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Betegnyilvantarto
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PeopleEditorWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PeopleEditorWindow : Window
     {
-        public MainWindow()
+        public PeopleEditorWindow()
         {
             InitializeComponent();
-        }
-
-        private void NewClient_Click(object sender, RoutedEventArgs e)
-        {
-            PeopleEditorWindow window = new PeopleEditorWindow();
-            window.ShowDialog();
         }
     }
 }
