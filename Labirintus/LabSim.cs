3054a957828475b6fb4d0eb112196e15e2b23097
Balu <Balu@DESKTOP-3S5SC9P>
2024-04-15 16:20:32 +0200

    Labirintus

---
+++ Labirintus/LabSim.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirintus
+{
+    internal class LabSim
+    {
+        KeresKesz {get; set} :bool
+        KijaratOszlopIndex
+    { get; }:int
+        KijaratSorIndex
+    { get; }:int
+        NincsMegoldas
+    { get; set }:bool
+        OszlopokSzama
+    { get; }:int
+        SorokSzama
+    { get; }:int
+
+
+    }
+private void BeolvasAdatsorok(string forras)
+    private void FeltoltLab()
+    public void KiirLab()
+    public LabSim(string forras)
+    public void Utkereses()
+
+}
