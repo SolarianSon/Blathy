63ed8830dc6046edc6c894db27f65953da75f22d
Balu <Balu@DESKTOP-3S5SC9P>
2024-04-22 17:08:54 +0200

    folytatas

--- Labirintus/LabSim.cs
+++ Labirintus/LabSim.cs
@@ -8,24 +8,67 @@ namespace Labirintus
 {
     internal class LabSim
     {
-        KeresKesz {get; set} :bool
-        KijaratOszlopIndex
-    { get; }:int
-        KijaratSorIndex
-    { get; }:int
-        NincsMegoldas
-    { get; set }:bool
-        OszlopokSzama
-    { get; }:int
-        SorokSzama
-    { get; }:int
+        List<string> Adatsorok = new List<string>();
+        Lab = char[,];
+
+        public bool KeresKesz { get; set};
+        public int KijaratOszlopIndex { get; };
+        public int KijaratSorIndex { get; };
+        public bool NincsMegoldas { get; set };
+        public int OszlopokSzama { get; };
+        public int SorokSzama { get; };
 
 
     }
-private void BeolvasAdatsorok(string forras)
+   private void BeolvasAdatsorok(string forras)
+{
+
+}
     private void FeltoltLab()
-    public void KiirLab()
-    public LabSim(string forras)
-    public void Utkereses()
+{
+
+}
+
+    public void KiirLab() 
+{
+
+}
+    public LabSim(string forras) 
+{
+
+}
+    public void Utkereses() 
+{
+    KeresesKesz = false;
+    NincsMegoldas = false;
+    byte r = 1, c = 0;
+
+    while (!KeresesKesz && !NincsMegoldas)
+    {
+        Lab[r, c] = 'O';
+            if (Lab[r, c + 1] = '')
+            {
+                c++;
+            }
+            else if (Lab[r + 1, c] = '')
+            {
+                r++;
+            }
+            else Lab[r, c] = '-';
+
+            if (Lab[r, c - 1] = 'O')
+            {
+                c--;
+            }
+            else r--;
+
+            KeresesKesz { r=KijaratSorIndex & c= KijaratOszlopIndex}
+            if (KeresesKesz)
+            {
+                Lab[r, c] = 'O';
+            }
+                NincsMegoldas { r=1 & c=0};
+    }
+}
 
 }
--- LabirintusGUI/MainWindow.xaml.cs
+++ LabirintusGUI/MainWindow.xaml.cs
@@ -20,5 +20,28 @@ namespace LabirintusGUI
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender , RoutedEventArgs e)
+        {
+            int rows = 12;
+            int cols = 12;
+
+            for (int row = 0; row < Height; row++)
+            {
+                for (int col = 0; col < Width; col++)
+                {
+                    CheckBox box = new CheckBox();
+                    if (row == 0)
+                    {
+                        box.IsChecked = true;
+                    }
+                    if (row == Height - 1)
+                    {
+                        box.IsChecked = true;
+                    }
+                    Lab.Children.Add(box);
+                }
+            }
+        }
     }
 }
